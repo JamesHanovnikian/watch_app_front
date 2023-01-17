2aa2c7748a2e572ac5801f8fd466c78e2b5702a2
YOUR NAME <jhanov@gmail.com>
2023-01-16 18:13:34 -0600

    adds create collector and watch to pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="home">
     <h2> Current Collectors </h2> 
-  <div v-for="collector in collectors" class="card" style="width: 18rem;">
+    <div v-for="collector in collectors" class="card" style="width: 18rem;">
       <img v-bind:src="collector.img_url" class="card-img-top" alt="Card image cap">
       <div class="card-body">
         <p class="card-text"> {{ collector.name }}
           {{ collector.age }} </p>
         <p class="card-text"> {{ collector.email }} </p> 
       </div>
-  </div>
+    </div>
+         <h3> Know a Collector? Add one!! </h3> 
 
-    <div v-for="collector in collectors">  
-      <button type="button" class="btn btn-primary">  {{ collector.name }} </button> 
-    </div> 
+         <div> 
+            Name: 
+            <input type="text" v-model="newCollectorParams.name" /> 
+            Email: 
+            <input type="text" v-model="newCollectorParams.email" /> 
+            Age: 
+            <input type="text" v-model="newCollectorParams.age" /> 
+            Image: 
+            <input type="text" v-model="newCollectorParams.img_url" /> 
+            <button v-on:click="createCollector()"> Add a Collector Profile </button> 
+          </div> 
+    </div>
 
-    
-  
-  </div>
+
 </template>
 <style>
 img {
@@ -31,6 +39,7 @@ export default {
     return {
       watches: [],
       collectors: [],
+      newCollectorParams: {},
     };
   },
   created: function () {
@@ -49,6 +58,18 @@ export default {
         console.log("collectors index", response);
         this.collectors = response.data;
       });
+    },
+    createCollector: function () {
+      axios
+        .post("/collectors", this.newCollectorParams)
+        .then((response) => {
+          console.log("make a collector!", response);
+          this.collectors.push(response.data);
+          this.newCollectorParams = {};
+        })
+        .catch((error) => {
+          console.log("new collector error", error.response);
+        });
     },
   },
 };
--- src/views/Watches.vue
+++ src/views/Watches.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="watches-index">
     <h2> All Watches </h2> 
+    
+    
     <div v-for="watch in watches" class="card" style="width: 18rem;">
         <img v-bind:src="watch.img_url" class="card-img-top" alt="Card image cap">
         <div class="card-body">
@@ -9,6 +11,21 @@
             {{ watch.model }}
         </div>
     </div>
+
+    <h3> Is your watch not featured? Add one here </h3> 
+    <div> 
+        Model: 
+        <input v-model="newWatchParams.model" type="text" />
+        Brand: 
+        <input v-model="newWatchParams.brand"type="text" />
+        Category: 
+        <input v-model="newWatchParams.category" type="text" />
+        Price: 
+        <input v-model="newWatchParams.price" type="text" />
+        Image Link: 
+        <input v-model="newWatchParams.img_url" type="text" /> 
+        <button v-on:click="createWatch()"> Add Watch! </button> 
+    </div> 
   </div>
 </template>
 <style>
@@ -30,6 +47,7 @@ export default {
     return {
       watches: [],
       collectors: [],
+      newWatchParams: {},
     };
   },
   created: function () {
@@ -48,6 +66,18 @@ export default {
         console.log("collectors index", response);
         this.collectors = response.data;
       });
+    },
+    createWatch: function () {
+      axios
+        .post("/watches", this.newWatchParams)
+        .then((response) => {
+          console.log("create watch", response);
+          this.watches.push(response.data);
+          this.newWatchParams = {};
+        })
+        .catch((error) => {
+          console.log("watches error", error.response);
+        });
     },
   },
 };
